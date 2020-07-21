7e45a8c42db659ef3854d9621a46bc2c8d83a9ce
TheHezze <48074682+TheHezze@users.noreply.github.com>
2020-07-20 21:57:02 -0400

    k

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,4 +134,17 @@
     items: 1
   });
 
+  // Modal popup$(function () {
+$('.portfolio-item').magnificPopup({
+  type: 'inline',
+  preloader: false,
+  focus: '#username',
+  modal: true
+});
+$(document).on('click', '.portfolio-modal-dismiss', function(e) {
+  e.preventDefault();
+  $.magnificPopup.close();
+});
+
+
 })(jQuery);
